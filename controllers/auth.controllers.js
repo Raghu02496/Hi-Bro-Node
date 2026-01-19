26abbb6163d58771dc936e8e6fd1d7bb2ce82cfe
Raghu02496 <lawlieta023@gmail.com>
2026-01-19 22:26:12 +0530

    feat: refresh endpoint added to handle refresh token

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,4 +1,4 @@
-import { userModel } from "../models/models.js"
+import { userModel, sessionModel } from "../models/models.js"
 import jwt from 'jsonwebtoken';
 import bcrypt from "bcryptjs";
 
@@ -18,9 +18,24 @@ export async function login(request, response){
             return response.status(400).json({ok : false, data : "Invalid password"})
         }
     
-        const token = jwt.sign({id : user._id}, process.env.JWT_SECRET, {expiresIn : '1h'})
+        const refreshToken = jwt.sign({ id : user._id }, process.env.REFRESH_SECRET, {expiresIn : '7d'})
+
+        const accessToken = jwt.sign({id : user._id}, process.env.JWT_SECRET, {expiresIn : '1h'})
+
+        const sessionData = await sessionModel.findOne({userId : user._id})
+
+        if(sessionData){
+            await sessionModel.deleteMany({ userId : user._id});
+        }
+
+        const session = new sessionModel({
+            userId : user._id,
+            refreshToken : bcrypt.hashSync(refreshToken, 10)
+        })
+
+        await session.save()
         
-        response.cookie("token",token,{
+        response.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: process.env.PROD,
             sameSite: 'None',
@@ -28,8 +43,31 @@ export async function login(request, response){
             path : '/'
         })
 
-        return response.json({ok : true, data : 'Sign in successful'})
+        return response.json({ok : true, accessToken : accessToken});
     }catch(error){
+        console.log(error,'error')
+        return response.status(500).json({ ok: false, error: error })
+    }
+}
+
+export async function refresh(request, response){
+    try {
+       const refreshToken = request.cookies.refreshToken;
+       if(!refreshToken){
+        response.status(401).json({ok: false, data: 'No refresh token found'});
+       }
+       const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+       const sessionData = await sessionModel.findOne({userId : decoded.id})
+
+       if(!sessionData){
+        response.status(401).json({ok: false, data: 'Not logged in'});
+       }
+       const valid = bcrypt.compareSync(refreshToken, sessionData.refreshToken);
+       if(!valid) return response.json({ok: false, data: "Invalid refresh token"})
+
+       const accessToken = jwt.sign({id : request.userId}, process.env.JWT_SECRET, { expiresIn : '1h'})
+       response.json({ ok: true, accessToken: accessToken});
+    } catch (error) {
         console.log(error,'error')
         return response.status(500).json({ ok: false, error: error })
     }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { login, logout } from "../controllers/auth.controllers.js";
+import { login, logout, refresh } from "../controllers/auth.controllers.js";
 
 const authRouter = express.Router();
 
 authRouter.post('/login',login)
 authRouter.post('/logout',logout)
+authRouter.post('/refresh',refresh)
 
 export default authRouter
