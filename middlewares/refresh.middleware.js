632b49ce3c258bab101dd9d088d6fbbd99b28cba
Raghu02496 <lawlieta023@gmail.com>
2026-01-20 21:01:00 +0530

    fix: return from function after sending response to client

--- middlewares/refresh.middleware.js
+++ middlewares/refresh.middleware.js
@@ -1,16 +1,16 @@
 import jwt from 'jsonwebtoken';
 
-export default async function refreshMiddleware(request, response, next){
-  
+export default async function refreshMiddleware(request, response, next) {
+
   try {
-        const refreshToken = request.cookies.refreshToken;
-        if(!refreshToken){
-            response.status(401).json({ok: false, data: 'No refresh token found'});
-        }
-        const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
-        request.userId = decoded.id
-        next();
-      } catch (err) {
-        response.status(403).json({ok : false, data: 'Invalid or expired token' });
-      }
+    const refreshToken = request.cookies.refreshToken;
+    if (!refreshToken) {
+      return response.status(401).json({ ok: false, data: 'No refresh token found' });
+    }
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+    request.userId = decoded.id
+    next();
+  } catch (err) {
+    return response.status(403).json({ ok: false, data: 'Invalid or expired token' });
+  }
 }
