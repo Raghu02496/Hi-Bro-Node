62bcb3bee883cc4449dd2882927823896af7ea6b
Raghu02496 <lawlieta023@gmail.com>
2026-01-20 23:02:18 +0530

    feat: seperate fn added to handle session delete code formatted

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -2,91 +2,91 @@ import { userModel, sessionModel } from "../models/models.js"
 import jwt from 'jsonwebtoken';
 import bcrypt from "bcryptjs";
 
-export async function login(request, response){
-    try{
-        const {userName, password} = request.body
-        
-        const user = await userModel.findOne({userName : userName})
-    
-        if(!user){
-            return response.status(400).json({ok : false, data : "User not found"})
+export async function login(request, response) {
+    try {
+        const { userName, password } = request.body
+
+        const user = await userModel.findOne({ userName: userName })
+
+        if (!user) {
+            return response.status(400).json({ ok: false, data: "User not found" })
         }
-    
+
         const valid = await bcrypt.compare(password, user.password)
-    
-        if(!valid){
-            return response.status(400).json({ok : false, data : "Invalid password"})
+
+        if (!valid) {
+            return response.status(400).json({ ok: false, data: "Invalid password" })
         }
-    
-        const refreshToken = jwt.sign({ id : user._id }, process.env.REFRESH_SECRET, {expiresIn : '7d'})
 
-        const accessToken = jwt.sign({id : user._id}, process.env.JWT_SECRET, {expiresIn : '1h'})
+        const refreshToken = jwt.sign({ id: user._id }, process.env.REFRESH_SECRET, { expiresIn: '7d' })
 
-        const sessionData = await sessionModel.findOne({userId : user._id})
+        const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
-        if(sessionData){
-            await sessionModel.deleteMany({ userId : user._id});
-        }
+        await deleteSession(user._id)
 
         const session = new sessionModel({
-            userId : user._id,
-            refreshToken : bcrypt.hashSync(refreshToken, 10)
+            userId: user._id,
+            refreshToken: bcrypt.hashSync(refreshToken, 10)
         })
 
         await session.save()
-        
+
         response.cookie("refreshToken", refreshToken, {
             httpOnly: true,
             secure: process.env.PROD,
             sameSite: 'None',
-            maxAge : 3600000,
-            path : '/'
+            maxAge: 3600000,
+            path: '/'
         })
 
-        return response.json({ok : true, accessToken : accessToken});
-    }catch(error){
-        console.log(error,'error')
+        return response.json({ ok: true, accessToken: accessToken });
+    } catch (error) {
+        console.log(error, 'error')
         return response.status(500).json({ ok: false, error: error })
     }
 }
 
-export async function refresh(request, response){
+export async function refresh(request, response) {
     try {
-       
-       const sessionData = await sessionModel.findOne({userId : request.userId})
 
-       if(!sessionData){
-        response.status(401).json({ok: false, data: 'Not logged in'});
-       }
-       const valid = bcrypt.compareSync(request.cookies.refreshToken, sessionData.refreshToken);
-       if(!valid) return response.json({ok: false, data: "Invalid refresh token"})
+        const sessionData = await sessionModel.findOne({ userId: request.userId })
 
-       const accessToken = jwt.sign({id : request.userId}, process.env.JWT_SECRET, { expiresIn : '1h'})
-       response.json({ ok: true, accessToken: accessToken});
+        if (!sessionData) {
+            response.status(401).json({ ok: false, data: 'Not logged in' });
+        }
+        const valid = bcrypt.compareSync(request.cookies.refreshToken, sessionData.refreshToken);
+        if (!valid) return response.json({ ok: false, data: "Invalid refresh token" })
+
+        const accessToken = jwt.sign({ id: request.userId }, process.env.JWT_SECRET, { expiresIn: '1h' })
+        response.json({ ok: true, accessToken: accessToken });
     } catch (error) {
-        console.log(error,'error')
+        console.log(error, 'error')
         return response.status(500).json({ ok: false, error: error })
     }
 }
 
 export async function logout(request, response) {
-    try{
-        response.clearCookie("accessToken",{
+    try {
+        response.clearCookie("accessToken", {
             httpOnly: true,
             secure: process.env.PROD,
             sameSite: 'None',
-            maxAge : 3600000,
-            path : '/'
+            maxAge: 3600000,
+            path: '/'
         });
 
-        const sessionData = await sessionModel.findOne({userId : request.userId})
-
-        if(sessionData){
-            await sessionModel.deleteMany({ userId : request.userId});
-        }
+        await deleteSession(request.userId)
         return response.json({ ok: true, message: "Logged out successfully" });
-    }catch(error){
-        console.log(error,'error')
+    } catch (error) {
+        console.log(error, 'error')
         return response.status(500).json({ ok: false, error: error })
+    }
+}
+
+async function deleteSession(userId) {
+    const sessionData = await sessionModel.findOne({ userId: userId })
+
+    if (sessionData) {
+        await sessionModel.deleteMany({ userId: userId });
     }
 }
