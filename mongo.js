11603a10cde85e940641be6e39d76a1d06885df7
Raghu02496 <lawlieta023@gmail.com>
2026-01-31 17:11:57 +0530

    fix: fixes runtime exit

--- mongo.js
+++ mongo.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 export default function connectMongo(){
     return mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log('Connected to MongoDB');
-    }).catch(()=>{
+    }).catch((err)=>{
         console.log('MongoDB connection failed:',err.message);
     });
 }
